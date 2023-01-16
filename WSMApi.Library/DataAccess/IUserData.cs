ebfdf759511daf4eeca4575abfe5c1cf6fa5326a
Timothy Tandt <Timothy.tandt@outlook.be>
2023-01-16 21:56:45 +0100

    Added UserData data access
    Added the Data Access to user table.

---
+++ WSMApi.Library/DataAccess/IUserData.cs
@@ -0,0 +1,12 @@
+﻿using WSMApi.Library.Models;
+
+namespace WSMApi.Library.DataAccess
+{
+    public interface IUserData
+    {
+        List<UserModel> GetUserById(string Id);
+        List<UserModel> GetUserByName(string FirstName, string LastName);
+        void InsertUser(UserModel user);
+        void UpdateUser(UserModel user);
+    }
+}
---
+++ WSMApi.Library/DataAccess/UserData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WSMApi.Library.Internal.DataAccess;
+using WSMApi.Library.Models;
+
+namespace WSMApi.Library.DataAccess;
+
+public class UserData : IUserData
+{
+    private readonly ISqlDataAccess _sql;
+
+    public UserData(ISqlDataAccess sql)
+    {
+        _sql = sql;
+    }
+
+    public List<UserModel> GetUserById(string Id)
+    {
+        var output = _sql.LoadData<UserModel, dynamic>("dbo.spUser_GetById", new { Id }, "WSMData");
+
+        return output;
+    }
+
+    public List<UserModel> GetUserByName(string FirstName, string LastName)
+    {
+        var output = _sql.LoadData<UserModel, dynamic>("dbo.spUser_GetByName", new { FirstName, LastName }, "WSMData");
+
+        return output;
+    }
+
+    public void InsertUser(UserModel user)
+    {
+        _sql.SaveData("dbo.spUser_Insert", user, "WSMData");
+    }
+
+    public void UpdateUser(UserModel user)
+    {
+        _sql.SaveData("dbo.spUser_Update", user, "WSMData");
+    }
+}
