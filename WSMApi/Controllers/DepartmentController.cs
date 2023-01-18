8c034a1f57f902c849281a5bd6d9fb8c6a040cff
Timothy Tandt <Timothy.tandt@outlook.be>
2023-01-18 13:32:52 +0100

    Added the Department Controller
    Added the SQL logic for the Department Table

---
+++ WSMApi/Controllers/DepartmentController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WSMApi.Library.DataAccess;
+using WSMApi.Library.Models;
+
+namespace WSMApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DepartmentController : ControllerBase
+{
+    private readonly IDepartmentData _departmentData;
+
+    public DepartmentController(IDepartmentData departmentData)
+	{
+        _departmentData = departmentData;
+    }
+
+    [HttpGet]
+    [Authorize]
+    [Route("GetDepartments")]
+    public List<DepartmentModel> Get()
+    {
+        return _departmentData.GetDepartments();
+    }
+
+    [HttpGet]
+    [Authorize]
+    [Route("GetDepartmentByName")] 
+    public List<DepartmentModel> GetDepartments(string DepartmentName)
+    {
+        return _departmentData.GetDepartmentByName(DepartmentName);
+    }
+
+    [HttpPost]
+    [Authorize]
+    [Route("InsertDepartment")]
+    public void InsertDepartment(DepartmentModel department)
+    {
+        _departmentData.InsertDepartment(department);
+    }
+
+    [HttpPost]
+    [Authorize]
+    [Route("UpdateDepartment")]
+    public void UpdateDepartment(DepartmentModel department) 
+    { 
+        _departmentData.UpdateDepartment(department);
+    }
+}
