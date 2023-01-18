9c6f5b611c3de3d8b332696bd3dca8fc58e891f2
Timothy Tandt <Timothy.tandt@outlook.be>
2023-01-18 13:08:45 +0100

    Added the CompanyController
    Added the logic for the Company Data Access

---
+++ WSMApi/Controllers/CompanyController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WSMApi.Library.DataAccess;
+using WSMApi.Library.Models;
+
+namespace WSMApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CompanyController : ControllerBase
+{
+    private readonly ICompanyData _companyData;
+
+    public CompanyController(ICompanyData companyData)
+	{
+        _companyData = companyData;
+    }
+
+    [HttpGet]
+    [Authorize]
+    [Route("GetCompanies")]
+    public List<CompanyModel> Get()
+    {
+        return _companyData.GetCompanies();
+    }
+
+    [HttpGet]
+    [Authorize]
+    [Route("GetCompanyByName")]
+    public List<CompanyModel> GetCompanyByName(string CompanyName)
+    {
+        return _companyData.GetCompanyByName(CompanyName);
+    }
+
+    [HttpPost]
+    [Authorize]
+    [Route("InsertCompany")]
+    public void InsertCompany(CompanyModel company)
+    {
+        _companyData.InsertCompany(company);
+    }
+
+    [HttpPost]
+    [Authorize]
+    [Route("UpdateCompany")]
+    public void UpdateCompany(CompanyModel company)
+    {
+        _companyData.UpdateCompany(company);
+    }
+}
