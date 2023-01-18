6e05420328bf7ea76905dbccb501f8224370800b
Timothy Tandt <Timothy.tandt@outlook.be>
2023-01-18 18:26:18 +0100

    Added the UI.Library Models
    Added the UI Models

---
+++ UI.Library/Models/CompanyModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Library.Models;
+
+public class CompanyModel
+{
+    public int Id { get; set; }
+    public string CompanyName { get; set; }
+    public string PhoneNumber { get; set; }
+    public int NumberOfEmployees { get; set; }
+    public string ChairPersonId { get; set; }
+    public string Description { get; set; }
+    public decimal Revenue { get; set; }
+    public decimal StockPrices { get; set; }
+    public DateTime DateFounded { get; set; }
+}
