de71a4aa532f33373bce8f3c16b6a090c66935e8
Timothy Tandt <Timothy.tandt@outlook.be>
2023-01-18 18:30:27 +0100

    Added the Authentication Models
    Added the models to say if you're logged in or not and updated the UserModel to have a reset method.

---
+++ UI.Library/Models/ILoggedInUserModel.cs
@@ -0,0 +1,18 @@
+﻿namespace UI.Library.Models
+{
+    public interface ILoggedInUserModel
+    {
+        int Age { get; set; }
+        DateTime CreatedDate { get; set; }
+        int DepartmentId { get; set; }
+        string EmailAddress { get; set; }
+        string FirstName { get; set; }
+        string Id { get; set; }
+        int JobTitleId { get; set; }
+        string LastName { get; set; }
+        string PhoneNumber { get; set; }
+        string Token { get; set; }
+
+        void ResetUserModel();
+    }
+}
--- UI.Library/Models/LoggedInUserModel.cs
+++ UI.Library/Models/LoggedInUserModel.cs
@@ -5,9 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace UI.Library.Models;
-
-public class UserModel
+public class LoggedInUserModel : ILoggedInUserModel
 {
+    public string Token { get; set; }
     public string Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
@@ -18,22 +18,17 @@ public class UserModel
     public int JobTitleId { get; set; }
     public DateTime CreatedDate { get; set; }
 
-    public Dictionary<string, string> Roles { get; set; } = new();
-    public List<JobTitleModel> JobTitles { get; set; } = new();
-
-    public string RoleList
+    public void ResetUserModel()
     {
-        get
-        {
-            return string.Join(", ", Roles.Select(x => x.Value));
-        }
-    }
-
-    public string JobList
-    {
-        get
-        {
-            return string.Join(", ", JobTitles.Select(x => x.JobName));
-        }
+        Token = "";
+        Id = "";
+        FirstName = "";
+        LastName = "";
+        EmailAddress = "";
+        PhoneNumber = "";
+        Age = 0;
+        DepartmentId = 0;
+        JobTitleId = 0;
+        CreatedDate = DateTime.MinValue;
     }
 }
