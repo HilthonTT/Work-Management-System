5598971295ca967d344dd21b3c7af8311b18e905
Timothy Tandt <Timothy.tandt@outlook.be>
2023-01-19 19:06:13 +0100

    Added the DepartmentEndpoint
    The DepartmentEndpoint can get/post information to the API

---
+++ UI.Library/API/DepartmentEndpoint.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI.Library.Models;
+
+namespace UI.Library.API;
+
+public class DepartmentEndpoint : IDepartmentEndpoint
+{
+    private readonly IAPIHelper _apiHelper;
+    private readonly ILogger<DepartmentEndpoint> _logger;
+
+    public DepartmentEndpoint(IAPIHelper apiHelper,
+                              ILogger<DepartmentEndpoint> logger)
+    {
+        _apiHelper = apiHelper;
+        _logger = logger;
+    }
+
+    public async Task<List<DepartmentModel>> GetAll()
+    {
+        using HttpResponseMessage response = await _apiHelper.ApiClient.GetAsync("api/Department/GetDepartments");
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await response.Content.ReadAsAsync<List<DepartmentModel>>();
+            return result;
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+
+    public async Task<List<DepartmentModel>> GetByName(string DepartmentName)
+    {
+        using HttpResponseMessage response = await _apiHelper.ApiClient.GetAsync("api/Department/GetDepartmentByName");
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await response.Content.ReadAsAsync<List<DepartmentModel>>();
+            return result;
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+
+    public async Task PostDepartment(DepartmentModel department)
+    {
+        using HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("api/Department/InsertDepartment", department);
+        if (response.IsSuccessStatusCode)
+        {
+            _logger.LogInformation("The department of name ({DepartmentName}) has successfully been added to the database.",
+                department.DepartmentName);
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+
+    public async Task UpdateDepartment(DepartmentModel department)
+    {
+        using HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("api/Department/UpdateDepartment", department);
+        if (response.IsSuccessStatusCode)
+        {
+            _logger.LogInformation("The department of Id ({Id}) has successfully been updated.", department.Id);
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+}
---
+++ UI.Library/API/IDepartmentEndpoint.cs
@@ -0,0 +1,12 @@
+﻿using UI.Library.Models;
+
+namespace UI.Library.API
+{
+    public interface IDepartmentEndpoint
+    {
+        Task<List<DepartmentModel>> GetAll();
+        Task<List<DepartmentModel>> GetByName(string DepartmentName);
+        Task PostDepartment(DepartmentModel department);
+        Task UpdateDepartment(DepartmentModel department);
+    }
+}
