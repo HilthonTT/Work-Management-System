64dae467b278f059630a607dbea04f70915f4ad3
Timothy Tandt <Timothy.tandt@outlook.be>
2023-01-19 18:35:47 +0100

    Added the CreateUserModel
    The model is used for the registering a user to the database.

---
+++ UI.Library/Models/CreateUserModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Library.Models;
+
+public class CreateUserModel
+{
+    [Required]
+    [DisplayName("First Name")]
+    public string FirstName { get; set; }
+
+    [Required]
+    [DisplayName("Last Name")]
+    public string LastName { get; set; }
+
+    [Required]
+    [DisplayName("Email Address")]
+    public string EmailAddress { get; set; }
+
+    [Required]
+    [DisplayName("Phone Number")]
+    public string PhoneNumber { get; set; }
+
+    [Required]
+    [DisplayName("Age")]
+    public int Age { get; set; }
+
+    [Required]
+    public int DepartmentId { get; set; }
+
+    [Required]
+    public int JobTitleId { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+
+    [Required]
+    [DisplayName("Confirm Password")]
+    [Compare(nameof(Password), ErrorMessage = "The passwords do not match.")]
+    public string ConfirmPassword { get; set; }
+}
