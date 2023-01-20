6d7abc39d8c5aab6428d9ec863d8e2c93b7288cf
Timothy Tandt <Timothy.tandt@outlook.be>
2023-01-20 19:53:30 +0100

    Updated the TaskEndpoint
    Changed the way we updated the Task Percentage

---
+++ UI.Library/API/ITaskEndpoint.cs
@@ -0,0 +1,14 @@
+﻿using UI.Library.Models;
+
+namespace UI.Library.API
+{
+    public interface ITaskEndpoint
+    {
+        Task<List<TaskModel>> GetAll();
+        Task<List<TaskModel>> GetByDepartmentId(string departmentId);
+        Task<List<TaskModel>> GetByUserId(string userId);
+        Task PostTask(TaskModel task);
+        Task Update(TaskModel task);
+        Task UpdatePercentage(TaskModel task);
+    }
+}
---
+++ UI.Library/API/TaskEndpoint.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI.Library.Models;
+
+namespace UI.Library.API;
+
+public class TaskEndpoint : ITaskEndpoint
+{
+    private readonly IAPIHelper _apiHelper;
+    private readonly ILogger<TaskEndpoint> _logger;
+
+    public TaskEndpoint(IAPIHelper apiHelper,
+                        ILogger<TaskEndpoint> logger)
+    {
+        _apiHelper = apiHelper;
+        _logger = logger;
+    }
+
+    public async Task<List<TaskModel>> GetAll()
+    {
+        using HttpResponseMessage response = await _apiHelper.ApiClient.GetAsync("api/Task/GetTasks");
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await response.Content.ReadAsAsync<List<TaskModel>>();
+
+            return result;
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+
+    public async Task<List<TaskModel>> GetByUserId(string userId)
+    {
+        using HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("api/Task/GetTasksByUserId", userId);
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await response.Content.ReadAsAsync<List<TaskModel>>();
+
+            return result;
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+
+    public async Task<List<TaskModel>> GetByDepartmentId(string departmentId)
+    {
+        using HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("api/Task/GetTasksByDepartmentId", departmentId);
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await response.Content.ReadAsAsync<List<TaskModel>>();
+
+            return result;
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+
+    public async Task PostTask(TaskModel task)
+    {
+        using HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("api/Task/InsertTask", task);
+        if (response.IsSuccessStatusCode)
+        {
+            _logger.LogInformation("The task of title ({Title}) has successfully been added to the database", task.Title);
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+
+    public async Task UpdatePercentage(TaskModel task)
+    {
+        using HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("api/Task/UpdateTaskPercentage", task);
+        if (response.IsSuccessStatusCode)
+        {
+            _logger.LogInformation("The task of Id ({Id}) has successfully been updated to the database", task.Id);
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+
+    public async Task Update(TaskModel task)
+    {
+        using HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("api/Task/UpdateTask", task);
+        if (response.IsSuccessStatusCode)
+        {
+            _logger.LogInformation("The task of Id ({Id}) has successfully been updated to the database", task.Id);
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+}
