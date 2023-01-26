d9130e38e3ead8dc6845ab3358375304615267c7
Timothy Tandt <Timothy.tandt@outlook.be>
2023-01-26 20:56:46 +0100

    Added the StockData
    Added the SQL data access for the Machine Table but is missing the SQL data access for the Part Table (Will be added later)

---
+++ WSMApi.Library/DataAccess/IStockData.cs
@@ -0,0 +1,14 @@
+﻿using WSMApi.Library.Models;
+
+namespace WSMApi.Library.DataAccess
+{
+    public interface IStockData
+    {
+        void DeleteMachine(int Id);
+        List<MachineModel> GetMachineById(int Id);
+        List<MachineModel> GetMachineByName(string MachineName);
+        List<MachineModel> GetMachines();
+        void InsertMachine(MachineModel machine);
+        void UpdateMachine(MachineModel machine);
+    }
+}
---
+++ WSMApi.Library/DataAccess/StockData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WSMApi.Library.Internal.DataAccess;
+using WSMApi.Library.Models;
+
+namespace WSMApi.Library.DataAccess;
+
+public class StockData : IStockData
+{
+    private readonly ISqlDataAccess _sql;
+
+    public StockData(ISqlDataAccess sql)
+    {
+        _sql = sql;
+    }
+
+    public List<MachineModel> GetMachines()
+    {
+        var output = _sql.LoadData<MachineModel, dynamic>("dbo.spMachine_GetAll", new { }, "WSMData");
+
+        return output;
+    }
+
+    public List<MachineModel> GetMachineByName(string MachineName)
+    {
+        var output = _sql.LoadData<MachineModel, dynamic>("dbo.spMachine_GetByName", new { MachineName }, "WSMData");
+
+        return output;
+    }
+
+    public List<MachineModel> GetMachineById(int Id)
+    {
+        var output = _sql.LoadData<MachineModel, dynamic>("dbo.spMachine_GetById", new { Id }, "WSMData");
+
+        return output;
+    }
+
+    public void InsertMachine(MachineModel machine)
+    {
+        _sql.SaveData("dbo.spMachine_Insert", machine, "WSMData");
+    }
+
+    public void UpdateMachine(MachineModel machine)
+    {
+        _sql.SaveData("dbo.spMachine_Update", machine, "WSMData");
+    }
+
+    public void DeleteMachine(int Id)
+    {
+        _sql.SaveData("dbo.spMachine_Delete", new { Id }, "WSMData");
+    }
+}
