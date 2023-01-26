dd0cf382354b8cefb45451f20ab9c2f4154f5799
Timothy Tandt <Timothy.tandt@outlook.be>
2023-01-26 20:57:06 +0100

    Added the Models for the Machine and Part Table

---
+++ WSMApi.Library/Models/MachineModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSMApi.Library.Models;
+
+public class MachineModel
+{
+    public int Id { get; set; }
+    public string MachineName { get; set; }
+    public string ModelName { get; set; }
+    public decimal EuropeanArticleNumber { get; set; }
+    public DateTime DatePurchased { get; set; }
+}
