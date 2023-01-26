a6cf4b304ffc50ff50596f227dc42ed5a138d9bd
Timothy Tandt <Timothy.tandt@outlook.be>
2023-01-26 20:25:37 +0100

    Added the Machine and Part Table

---
+++ WSMData/dbo/Tables/Machine.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Machine]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [MachineName] NVARCHAR(100) NOT NULL, 
+    [ModelName] NVARCHAR(255) NOT NULL, 
+    [EuropeanArticleNumber] NVARCHAR(50) NOT NULL,
+    [PurchasedPrice] MONEY NOT NULL, 
+    [DatePurchased] DATETIME2 NOT NULL
+)
---
+++ WSMData/dbo/Tables/Part.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Part]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [PartName] NVARCHAR(100) NOT NULL,
+    [PartModel] NVARCHAR(255) NOT NULL, 
+    [MachineId] INT NULL,
+    [DatePurchased] DATETIME2 NOT NULL, 
+    CONSTRAINT [FK_Part_ToMachine] FOREIGN KEY (MachineId) REFERENCES Machine(Id) ON DELETE CASCADE
+)
