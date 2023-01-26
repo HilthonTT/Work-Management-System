91f2eca8eaed5c85e97a3edf0d37462f6a1268c3
Timothy Tandt <Timothy.tandt@outlook.be>
2023-01-26 20:34:05 +0100

    Changed PartModel to ModelName in Part Table

--- WSMData/dbo/Tables/Part.sql
+++ WSMData/dbo/Tables/Part.sql
@@ -2,7 +2,7 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [PartName] NVARCHAR(100) NOT NULL,
-    [PartModel] NVARCHAR(255) NOT NULL, 
+    [ModelName] NVARCHAR(255) NOT NULL, 
     [MachineId] INT NULL,
     [DatePurchased] DATETIME2 NOT NULL, 
     CONSTRAINT [FK_Part_ToMachine] FOREIGN KEY (MachineId) REFERENCES Machine(Id) ON DELETE CASCADE
