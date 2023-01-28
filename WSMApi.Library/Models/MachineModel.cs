0e2648a4bd549ff7b3635e66146936370fb3e428
Timothy Tandt <Timothy.tandt@outlook.be>
2023-01-28 17:39:15 +0100

    Fixed a bug where the EAN field was a decimal and not a string

--- WSMApi.Library/Models/MachineModel.cs
+++ WSMApi.Library/Models/MachineModel.cs
@@ -11,6 +11,6 @@ public class MachineModel
     public int Id { get; set; }
     public string MachineName { get; set; }
     public string ModelName { get; set; }
-    public decimal EuropeanArticleNumber { get; set; }
+    public string EuropeanArticleNumber { get; set; }
     public DateTime DatePurchased { get; set; }
 }
