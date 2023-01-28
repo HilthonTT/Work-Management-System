81a4c82048c9a75be0fe0500937658c9fbc56bf4
Timothy Tandt <Timothy.tandt@outlook.be>
2023-01-28 14:05:36 +0100

    Added the StockEndpoint
    StockEndpoint communicates with the API to do CRUD operations

---
+++ UI.Library/API/IStockEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using UI.Library.Models;
+
+namespace UI.Library.API
+{
+    public interface IStockEndpoint
+    {
+        Task DeleteMachine(int Id);
+        Task DeletePart(int Id);
+        Task<List<MachineModel>> GetAllMachines();
+        Task<List<PartModel>> GetAllParts();
+        Task<List<MachineModel>> GetMachineById(int Id);
+        Task<List<MachineModel>> GetMachineByModelName(string ModelName);
+        Task<List<PartModel>> GetPartById(int Id);
+        Task<List<PartModel>> GetPartByModelName(string ModelName);
+        Task InsertMachine(MachineModel machine);
+        Task InsertPart(PartModel part);
+        Task UpdateMachine(MachineModel machine);
+        Task UpdatePart(PartModel part);
+    }
+}
---
+++ UI.Library/API/StockEndpoint.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI.Library.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace UI.Library.API;
+
+public class StockEndpoint : IStockEndpoint
+{
+    private readonly IAPIHelper _apiHelper;
+    private readonly ILogger _logger;
+
+    public StockEndpoint(IAPIHelper apiHelper,
+                         ILogger logger)
+    {
+        _apiHelper = apiHelper;
+        _logger = logger ?? NullLogger<StockEndpoint>.Instance;
+    }
+
+    // Machine section //
+
+    public async Task<List<MachineModel>> GetAllMachines()
+    {
+        using HttpResponseMessage response = await _apiHelper.ApiClient.GetAsync("api/Stock/GetMachines");
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await response.Content.ReadAsAsync<List<MachineModel>>();
+
+            return result;
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+
+    public async Task<List<MachineModel>> GetMachineByModelName(string ModelName)
+    {
+        var data = new { ModelName };
+
+        using HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("api/Stock/GetMachineByModelName", data);
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await response.Content.ReadAsAsync<List<MachineModel>>();
+
+            return result;
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+
+    public async Task<List<MachineModel>> GetMachineById(int Id)
+    {
+        var data = new { Id };
+
+        using HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("api/Stock/GetMachineById", data);
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await response.Content.ReadAsAsync<List<MachineModel>>();
+
+            return result;
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+
+    public async Task InsertMachine(MachineModel machine)
+    {
+        using HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("api/Stock/InsertMachine", machine);
+        if (response.IsSuccessStatusCode)
+        {
+            _logger.LogInformation("The machine of ModelName {ModelName} has been added to the database", machine.ModelName);
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+
+    public async Task UpdateMachine(MachineModel machine)
+    {
+        using HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("api/Stock/UpdateMachine", machine);
+        if (response.IsSuccessStatusCode)
+        {
+            _logger.LogInformation("The machine of Id {MachineId} has been updated in the database", machine.Id);
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+
+    public async Task DeleteMachine(int Id)
+    {
+        var data = new { Id };
+
+        using HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("api/Stock/DeleteMachine", data);
+        if (response.IsSuccessStatusCode)
+        {
+            _logger.LogInformation("The machine of Id {Id} has been deleted from the database.", data.Id);
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+
+    // Part section //
+
+    public async Task<List<PartModel>> GetAllParts()
+    {
+        using HttpResponseMessage response = await _apiHelper.ApiClient.GetAsync("api/Stock/GetParts");
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await response.Content.ReadAsAsync<List<PartModel>>();
+
+            return result;
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+
+    public async Task<List<PartModel>> GetPartByModelName(string ModelName)
+    {
+        var data = new { ModelName };
+
+        using HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("api/Stock/GetPartByModelName", data);
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await response.Content.ReadAsAsync<List<PartModel>>();
+
+            return result;
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+
+    public async Task<List<PartModel>> GetPartById(int Id)
+    {
+        var data = new { Id };
+
+        using HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("api/Stock/GetPartById", data);
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await response.Content.ReadAsAsync<List<PartModel>>();
+
+            return result;
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+
+    public async Task InsertPart(PartModel part)
+    {
+        using HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("api/Stock/InsertPart", part);
+        if (response.IsSuccessStatusCode)
+        {
+            _logger.LogInformation("The part of ModelName {ModelName} has been added the database", part.ModelName);
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+
+    public async Task UpdatePart(PartModel part)
+    {
+        using HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("api/Stock/DeletePart", part);
+        if (response.IsSuccessStatusCode)
+        {
+            _logger.LogInformation("The part of Id {Id} and of ModelName {ModelName} has been updated in the database", part.Id, part.ModelName);
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+
+    public async Task DeletePart(int Id)
+    {
+        var data = new { Id };
+
+        using HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("api/Stock/InsertPart", data);
+        if (response.IsSuccessStatusCode)
+        {
+            _logger.LogInformation("The part of Id {Id} has been deleted database", data.Id);
+        }
+        else
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+    }
+}
