d661d3a39016ed7156839a7a171f3dea36437eee
Timothy Tandt <Timothy.tandt@outlook.be>
2023-01-28 14:02:57 +0100

    Added the MachineModel and PartModel to the UI Library

---
+++ UI.Library/Models/PartModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Library.Models;
+
+public class PartModel
+{
+    public int Id { get; set; }
+    public string ModelName { get; set; }
+    public int? MachineId { get; set; }
+    public DateTime DatePurchased { get; set; }
+}
