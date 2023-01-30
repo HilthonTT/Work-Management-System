44740950a5dc111fe58a96c4b2aa22836cb6e146
Timothy Tandt <Timothy.tandt@outlook.be>
2023-01-30 19:48:02 +0100

    Added the Stored Procedure to update the User JobTitleId

---
+++ WSMData/dbo/Stored Procedure/spUser_UpdateJobId.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spUser_UpdateJobId]
+	@Id nvarchar(128),
+	@JobTitleId int
+AS
+begin
+	set nocount on;
+
+	UPDATE [dbo].[User] set 
+	JobTitleId = @JobTitleId
+
+	where Id = @Id
+end
