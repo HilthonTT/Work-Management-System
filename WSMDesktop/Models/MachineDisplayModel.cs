e6599f86a86d43fd3bc52a78a8824eb4e407f0c0
Timothy Tandt <Timothy.tandt@outlook.be>
2023-02-01 15:07:36 +0100

    Missing Field in MachineModels
    Fixed a bug where the PurchasedPrice field was missing in all the MachineModels.

--- WSMDesktop/Models/MachineDisplayModel.cs
+++ WSMDesktop/Models/MachineDisplayModel.cs
@@ -11,6 +11,7 @@ public class MachineDisplayModel
     public int Id { get; set; }
     public string MachineName { get; set; }
     public string ModelName { get; set; }
+    public decimal PurchasedPrice { get; set; }
     public string EuropeanArticleNumber { get; set; }
     public DateTime DatePurchased { get; set; }
 
