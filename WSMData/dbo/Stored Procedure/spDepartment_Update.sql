4498fb68446ccf712615874cfddf1d9fa5feb618
Timothy Tandt <Timothy.tandt@outlook.be>
2023-02-05 19:03:28 +0100

    A parameter was missing the spDepartment_Update

--- WSMData/dbo/Stored Procedure/spDepartment_Update.sql
+++ WSMData/dbo/Stored Procedure/spDepartment_Update.sql
@@ -5,7 +5,8 @@
 	@Address nvarchar(255),
 	@ChairPersonId nvarchar(128),
 	@PhoneNumber nvarchar(50),
-	@Description nvarchar(MAX)
+	@Description nvarchar(MAX),
+	@CreatedDate datetime2
 
 AS
 begin
@@ -17,7 +18,8 @@ begin
 	[Address] = @Address,
 	ChairPersonId = @ChairPersonId,
 	PhoneNumber = @PhoneNumber,
-	[Description] = @Description
+	[Description] = @Description,
+	CreatedDate = @CreatedDate
 
 	WHERE Id = @Id
 end
