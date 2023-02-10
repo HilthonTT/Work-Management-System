065689e71b1aa85c7cb75ac692bd3edac5e558b8
Timothy Tandt <Timothy.tandt@outlook.be>
2023-02-10 20:57:43 +0100

    Added the the CreateTask Page
    The page makes sure to it checks that both the userId and DepartmentId aren't null before posting it to the database and a CreateTaskModel was created to use DataAnnotation

---
+++ WSMPortal/Models/CreateTaskModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WSMPortal.Models;
+
+public class CreateTaskModel
+{
+    public int Id { get; set; }
+    public string? UserId { get; set; }
+    public int? DepartmentId { get; set; }
+
+    [Required(ErrorMessage = "Title is a required field.")]
+    public string Title { get; set; }
+
+    [Required(ErrorMessage = "Description is a required field.")]
+    public string Description { get; set; }
+
+    [Required]
+    [DisplayName("Date Due")]
+    [Range(typeof(DateTime), "01/01/1753", "31/12/9999", ErrorMessage = "DateTime must be between 01/01/1753 and 31/12/9999")]
+    public DateTime DateDue { get; set; } = DateTime.UtcNow;
+    public int PercentageDone { get; set; }
+    public bool IsDone { get; set; }
+    public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+}
