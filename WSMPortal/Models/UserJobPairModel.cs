f6b0bf1335590775171f7a09400a44b3810e47ee
Timothy Tandt <Timothy.tandt@outlook.be>
2023-02-12 14:40:19 +0100

    Added UserRolePairModel and UserRolePairModel
    The models are used in the EditForm to Add/Remove a Role/Job

---
+++ WSMPortal/Models/UserJobPairModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WSMPortal.Models;
+
+public class UserJobPairModel
+{
+    [Required(ErrorMessage = "A User Must Be Selected.")]
+    public string Id { get; set; }
+    [Required(ErrorMessage = "A Job Must Be Selected.")]
+    public int JobId { get; set; }
+}
---
+++ WSMPortal/Models/UserRolePairModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WSMPortal.Models;
+
+public class UserRolePairModel
+{
+    [Required(ErrorMessage = "A user must be selected.")]
+    public string Id { get; set; }
+
+    [Required(ErrorMessage = "A role must be selected.")]
+    public string RoleName { get; set; }
+}
