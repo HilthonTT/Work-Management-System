aec7adf0aa54992de38805efa6461312a7fe27e7
Timothy Tandt <Timothy.tandt@outlook.be>
2023-02-15 19:49:25 +0100

    Updated DepartmentModel
    Changed ChairpersonId to be nullable

--- UI.Library/Models/DepartmentModel.cs
+++ UI.Library/Models/DepartmentModel.cs
@@ -12,7 +12,7 @@ public class DepartmentModel
     public int CompanyId { get; set; }
     public string DepartmentName { get; set; }
     public string Address { get; set; }
-    public string ChairPersonId { get; set; }
+    public string? ChairPersonId { get; set; }
     public string PhoneNumber { get; set; }
     public string Description { get; set; }
     public DateTime CreatedDate { get; set; }
