a5b7f0dd376b0212040d0a260eb8bf923c67e4de
Timothy Tandt <Timothy.tandt@outlook.be>
2023-02-15 13:42:38 +0100

    Renamed CreateTaskModel to FormTaskModel

--- WSMPortal/Models/FormTaskModel.cs
+++ WSMPortal/Models/FormTaskModel.cs
@@ -3,21 +3,21 @@ using System.ComponentModel.DataAnnotations;
 
 namespace WSMPortal.Models;
 
-public class CreateTaskModel
+public class FormTaskModel
 {
     public int Id { get; set; }
     public string? UserId { get; set; }
     public int? DepartmentId { get; set; }
 
-    [Required(ErrorMessage = "Title is a required field.")]
+    [Required(ErrorMessage = "Title Is A Required Field.")]
     public string Title { get; set; }
 
-    [Required(ErrorMessage = "Description is a required field.")]
+    [Required(ErrorMessage = "Description Is A Required Field.")]
     public string Description { get; set; }
 
     [Required]
     [DisplayName("Date Due")]
-    [Range(typeof(DateTime), "01/01/1753", "31/12/9999", ErrorMessage = "DateTime must be between 01/01/1753 and 31/12/9999")]
+    [Range(typeof(DateTime), "01/01/1753", "31/12/9999", ErrorMessage = "DateTime Must Be Between 01/01/1753 And 31/12/9999")]
     public DateTime DateDue { get; set; } = DateTime.UtcNow;
     public int PercentageDone { get; set; }
     public bool IsDone { get; set; }
