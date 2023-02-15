a17ac7bd0c447b2b9f5466f7cdfc7677f55d1968
Timothy Tandt <Timothy.tandt@outlook.be>
2023-02-15 13:43:22 +0100

    Added FormCompanyModel
    Used for Blazor EditForm as a Model.

---
+++ WSMPortal/Models/FormCompanyModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WSMPortal.Models;
+
+public class FormCompanyModel
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Company Name Is A Required Field.")]
+    [DisplayName("Company Name")]
+    public string CompanyName { get; set; }
+
+    [Required(ErrorMessage = "The Address Is A Required Field.")]
+    public string Address { get; set; }
+
+    [Required(ErrorMessage = "The Phone Number Is A Required Field.")]
+    [DisplayName("Phone Number")]
+    public string PhoneNumber { get; set; }
+    public string? ChairPersonId { get; set; }
+
+    [Required(ErrorMessage = "The Description Is A Required Field.")]
+    public string Description { get; set; }
+
+    [Required(ErrorMessage = "The Date Founded Is A Required Field.")]
+    [DisplayName("Date Founded")]
+    public DateTime DateFounded { get; set; }
+}
