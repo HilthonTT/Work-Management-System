4f8e85757991a059bb5cad5c328bdb30e93c0a3f
Timothy Tandt <Timothy.tandt@outlook.be>
2023-02-18 13:52:20 +0100

    Added FormPartModel
    Added the FormPartModel to be used in an EditForm

---
+++ WSMPortal/FormModels/FormPartModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WSMPortal.FormModels;
+
+public class FormPartModel
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Part Name Is A Required Field.")]
+    [DisplayName("Part Name")]
+    public string PartName { get; set; }
+
+    [Required(ErrorMessage = "Model Name Is A Required Field.")]
+    [DisplayName("Model Name")]
+    public string ModelName { get; set; }
+
+    [Required(ErrorMessage = "Purchased Price Is A Required Field.")]
+    [DisplayName("Purchased Price")]
+    public decimal PurchasedPrice { get; set; }
+
+    [DisplayName("Selected Machine")]
+    public int? MachineId { get; set; }
+
+    [Required(ErrorMessage = "Date Purchased Is A Required Field.")]
+    [DisplayName("Date Purchased")]
+    [Range(typeof(DateTime), "01/01/1753", "31/12/9999", ErrorMessage = "DateTime Must Be Between 01/01/1753 And 31/12/9999")]
+    public DateTime DatePurchased { get; set; }
+}
