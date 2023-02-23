986c2cefd7e45581df19c855e45c850c05602324
Timothy Tandt <Timothy.tandt@outlook.be>
2023-02-23 18:45:56 +0100

    Created the CreateTask.razor
    The page allows admins to create a task, the task contains a Title, Description and department/users you can add.

---
+++ WSMPortal/Models/CreateTaskModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WSMPortal.Models;
+
+public class CreateTaskModel
+{
+
+    [Required]
+    [DisplayName("User")]
+    public string? UserId { get; set; }
+
+    [Required]
+    [DisplayName("Department")]
+    public int? DepartmentId { get; set; }
+
+    [Required]
+    [MaxLength(75)]
+    public string Title { get; set; }
+
+    [Required]
+    [MaxLength(500)]
+    public string Description { get; set; }
+
+    [Required]
+    [DisplayName("Date Due")]
+    public DateTime DateDue { get; set; }
+
+    public int PercentageDone { get; set; }
+    public bool IsDone { get; set; }
+    public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+    public bool Archived { get; set; }
+}
