83982aeaa4915f71f06b4334f077a5c73d2d8b3a
Timothy Tandt <Timothy.tandt@outlook.be>
2023-02-27 20:55:27 +0100

    Added the CreateReport.razor
    The CreareReport page is for making a report on the progress of the task.

---
+++ WSMPortal/Models/CreateReportModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WSMPortal.Models;
+
+public class CreateReportModel
+{
+    public int Id { get; set; }
+
+    [Required]
+    [DisplayName("Task")]
+    public int? TaskId { get; set; }
+
+    [DisplayName("Author")]
+    public string UserId { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Title { get; set; }
+
+    [Required]
+    [MaxLength(500)]
+    public string Description { get; set; }
+    public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+    public bool Archived { get; set; } = false;
+}
