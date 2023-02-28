04d37bcbe8e6b7cc4e2798e868908a0cb571c52b
Timothy Tandt <Timothy.tandt@outlook.be>
2023-02-28 13:41:17 +0100

    Added the ModifyPercentage.razor
    This page allows you to modify the progress fo the task if you're in the department.

---
+++ WSMPortal/Models/ModifyPercentageTaskModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WSMPortal.Models;
+
+public class ModifyPercentageTaskModel
+{
+    [Required]
+    [Range(0, 100, ErrorMessage = "The Progress of the task must be greater than 1 and lower than 100.")]
+    public int PercentageDone { get; set; }
+}
